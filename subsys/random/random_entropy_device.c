8811af991f90ed5e3d7c8050c0c225b3adb14af9
Jan Müller <jan.mueller@nordicsemi.no>
2024-06-06 16:17:59 +0200

    dummy build for flpr

--- subsys/random/random_entropy_device.c
+++ subsys/random/random_entropy_device.c
@@ -9,51 +9,47 @@
 #include <zephyr/drivers/entropy.h>
 #include <string.h>
 
-static const struct device *const entropy_dev =
-	DEVICE_DT_GET(DT_CHOSEN(zephyr_entropy));
+//static const struct device *const entropy_dev =
+//	DEVICE_DT_GET(DT_CHOSEN(zephyr_entropy));
+
+static inline uint32_t m_rotl(const uint32_t x, int k)
+{
+  return (x << k) | (x >> (32 - k));
+}
+
+static uint32_t m_get(void)
+{
+
+  static uint32_t s0     = 5;
+  static uint32_t s1     = 8;
+  const uint32_t result = m_rotl(s0 * 0x9E3779BB, 5) * 5;
+
+  s1 ^= s0;
+  s0 = m_rotl(s0, 26) ^ s1 ^ (s1 << 9);
+  s1 = m_rotl(s1, 13);
+
+  return result;
+}
+
+static uint8_t rand_prio_low_vector_get(uint8_t *p_buff, uint8_t length)
+{
+	for(uint8_t i=0; i<length; ++i)
+	{
+		p_buff[i] = m_get() & 0xff;
+	}
+	return length;
+}
+
+static void rand_prio_low_vector_get_blocking(uint8_t *p_buff, uint8_t length)
+{
+	(void) rand_prio_low_vector_get(p_buff, length);
+}
 
 static int rand_get(uint8_t *dst, size_t outlen, bool csrand)
 {
-	uint32_t random_num;
-	int ret;
+	(void) csrand;
 
-	__ASSERT(device_is_ready(entropy_dev), "Entropy device %s not ready",
-		 entropy_dev->name);
-
-	ret = entropy_get_entropy(entropy_dev, dst, outlen);
-
-	if (unlikely(ret < 0)) {
-		/* Don't try to fill the buffer in case of
-		 * cryptographically secure random numbers, just
-		 * propagate the driver error.
-		 */
-		if (csrand) {
-			return ret;
-		}
-
-		/* Use system timer in case the entropy device couldn't deliver
-		 * 32-bit of data.  There's not much that can be done in this
-		 * situation.  An __ASSERT() isn't used here as the HWRNG might
-		 * still be gathering entropy during early boot situations.
-		 */
-
-		uint32_t len = 0;
-		uint32_t blocksize = 4;
-
-		while (len < outlen) {
-			size_t copylen = outlen - len;
-
-			if (copylen > blocksize) {
-				copylen = blocksize;
-			}
-
-			random_num = k_cycle_get_32();
-			(void)memcpy(&(dst[len]), &random_num, copylen);
-			len += copylen;
-		}
-	}
-
-	return 0;
+	return rand_prio_low_vector_get(dst, outlen);
 }
 
 #if defined(CONFIG_ENTROPY_DEVICE_RANDOM_GENERATOR)
